81b1f01968d2cea210c5194a511e963a6515c0c9
Christian Sack <christian@sack.at>
2023-03-03 09:48:52 +0100

    changed to german

--- src/Solution/web.app/Program.cs
+++ src/Solution/web.app/Program.cs
@@ -1,6 +1,10 @@
 global using System.Collections;
 global using web.app._abstractions;
 global using web.app.generators;
+using System.Globalization;
+
+Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("de-AT");
+Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("de-AT");
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
